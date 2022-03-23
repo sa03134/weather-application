65b8257c42bf76f6a3fb9a83e0cafda953903304
sa03134 <jungwoosohn71@gmail.com>
2022-03-23 23:38:06 +0900

    style : construct UI design using proportion and APIs

--- NomadWeather/App.js
+++ NomadWeather/App.js
@@ -1,6 +1,67 @@
+import { StatusBar } from "expo-status-bar";
 import { React } from "react";
-import { View } from "react-native";
+import { Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
-  return <View></View>;
+  return (
+    <View style={styles.container}>
+      <StatusBar style="dark"></StatusBar>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Daegu</Text>
+      </View>
+      <ScrollView
+        showsHorizontalScrollIndicator={false}
+        pagingEnabled
+        horizontal
+        contentContainerStyle={styles.weather}
+      >
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: "rgb(214,230,245)" },
+  city: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: { fontSize: 68, fontWeight: "bold" },
+  weather: {},
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "center",
+    // backgroundColor: "rgb(239, 255, 253)",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 140,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
+  },
+});
